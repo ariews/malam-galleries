909e72b127bdbb7678361f52ac1d5ff6feca7f72
Arie W. Subagja <arie@malam.or.id>
2013-09-04 22:59:26 +0700

    okeh, update jadi bener

--- classes/malam/controller/admin/gallery.php
+++ classes/malam/controller/admin/gallery.php
@@ -9,15 +9,51 @@ defined('SYSPATH') or die('No direct script access.');
 abstract class Malam_Controller_Admin_Gallery extends Controller_Abstract_Bigcontent
 {
     /**
-     * Bigcontent
+     * Gallery
      *
      * @var Model_Gallery
      */
-    protected $model    = 'gallery';
+    protected $model            = 'gallery';
+
+    /**
+     * Bigcontent
+     *
+     * @var Model_Bigcontent
+     */
+    protected $content_id       = NULL;
 
     public function __construct(Request $request, Response $response)
     {
+        $model    = $request->param('model');
+        $model_id = $request->param('model_id');
+
+        $gallery  = ORM::factory('gallery');
+        /* @var $gallery Model_Gallery */
+
+        $model_loaded = $model && $model_id;
+
+        if ($model_loaded)
+        {
+            $content = $this->content = ORM::factory($model)
+                    ->find_by_id($model_id)->find();
+
+            if (! $content->loaded())
+            {
+                throw new HTTP_Exception_404();
+            }
+
+            $this->content_id = $content->pk();
+            $gallery->set_content($content);
+        }
+
+        $this->model = $gallery->where('content_id', '=', $this->content_id);
+
         parent::__construct($request, $response);
+
+        if ($this->model->bigcontent->loaded() && !($model_loaded))
+        {
+            throw new HTTP_Exception_404();
+        }
     }
 
     public function action_index()
@@ -38,5 +74,14 @@ abstract class Malam_Controller_Admin_Gallery extends Controller_Abstract_Bigcon
     public function action_delete()
     {
         $this->title('Delete Gallery');
+    }
+
+    protected function _create_or_update()
+    {
+        $this->temporary->set(array(
+            'content_id' => $this->content_id
+        ));
+
+        parent::_create_or_update();
     }
 }
--- classes/malam/model/gallery.php
+++ classes/malam/model/gallery.php
@@ -56,7 +56,7 @@ abstract class Malam_Model_Gallery extends Model_Bigcontent
      */
     protected $_belongs_to      = array(
         'user'          => array('model' => 'user', 'foreign_key' => 'user_id'),
-        'bigcontent'    => array('model' => 'bigcontent', 'foreign_key' => 'hierarchy_id'),
+        'bigcontent'    => array('model' => 'bigcontent', 'foreign_key' => 'content_id'),
     );
 
     /**
@@ -75,6 +75,26 @@ abstract class Malam_Model_Gallery extends Model_Bigcontent
     protected $_tag_enable      = FALSE;
     protected $_is_direct_call  = FALSE;
 
+    /**
+     * Filter definitions for validation
+     *
+     * @return array
+     */
+    public function filters()
+    {
+        return parent::filters() + array(
+            'content_id' => array(
+                array('ORM::Check_Model', array(':value', 'bigcontent')),
+            )
+        );
+    }
+
+    public function set_content(Model_Bigcontent $content)
+    {
+        $this->bigcontent = $content;
+        return $this;
+    }
+
     public function to_paginate()
     {
         return Paginate::factory($this)
@@ -91,12 +111,97 @@ abstract class Malam_Model_Gallery extends Model_Bigcontent
                 break;
 
             case 'photos':
-                return $this->total_photos();
+                return $this->images
+                        ->set_content($this)
+                        ->gallery_index_url($this->total_photos());
                 break;
 
             default :
                 return parent::get_field($field);
                 break;
         endswitch;
+    }
+
+    protected function link($action = 'index', $title = NULL, array $params = NULL, array $attributes = NULL, array $query = NULL)
+    {
+        empty($params) && $params = array();
+
+        $model_id = $model = NULL;
+
+        if ($this->bigcontent->loaded())
+        {
+            $model_id = $this->bigcontent->pk();
+            $model    = $this->bigcontent->object_name();
+        }
+
+        $params += array('model_id' => $model_id, 'model' => $model);
+
+        return parent::link($action, $title, $params, $attributes, $query);
+    }
+
+    protected function prepare_menu()
+    {
+        $menu = array();
+
+        if ($this->bigcontent->loaded())
+        {
+            $menu[] = array(
+                'title' => __(ORM::capitalize_title($this->bigcontent->object_name())),
+                'url'   => $this->bigcontent->admin_update_url_only(),
+            );
+        }
+
+        $menu = array_merge($menu, array(
+            array(
+                'title' => __('Galleries'),
+                'url'   => $this->admin_index_url_only(),
+            ),
+            array(
+                'title' => __($this->loaded() ? 'Update' : 'Add'),
+                'url'   => $this->loaded()
+                            ? $this->admin_update_url_only()
+                            : $this->admin_create_url_only()
+            ),
+        ));
+
+        if ($this->loaded() && $this->image_enable())
+        {
+            $menu[] = array(
+                'title' => __('Images'),
+                'url'   => $this->images
+                                ->set_content($this)
+                                ->gallery_index_url_only(),
+            );
+        }
+
+        $this->_admin_menu = $menu;
+    }
+
+    /**
+     * Set values from an array with support for one-one relationships.  This method should be used
+     * for loading in post data, etc.
+     *
+     * @param  array $values   Array of column => val
+     * @param  array $expected Array of keys to take from $values
+     * @return ORM
+     */
+    public function values(array $values, array $expected = NULL)
+    {
+        if (isset($values['content_id']) && ($values['content_id'] == '0' || empty($values['content_id'])))
+        {
+            unset($values['content_id']);
+        }
+
+        if (NULL === $expected || empty($expected))
+        {
+            $expected = array('user_id', 'title', 'content', 'state', 'content_id');
+
+            if ($this->featured_enable())
+            {
+                $expected[] = 'is_featured';
+            }
+        }
+
+        return parent::values($values, $expected);
     }
 }
